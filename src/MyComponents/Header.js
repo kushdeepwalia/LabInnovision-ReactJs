71434227a3c364280e9de35c9575af90b556e328
kushdeepwalia <skushdeep5@gmail.com>
2021-07-19 19:29:19 +0530

    'Few-changes-made'

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,27 +1,64 @@
-import React from 'react'
-import logo from '../Images/logo.svg';
-import './css/header.css'
+import React, { useState } from "react";
+import logo from "../Images/logo.svg";
+import "./css/header.css";
 import { Link } from "react-router-dom";
 
 export const Header = (props) => {
-    return (
-        <div>
-            <header className="header bb-8-green">
-                <div className="webName">
-                    {props.logo ? <div className="logo"><img src={logo} alt="Website Logo"/></div> : "" }
-                    <div className="web-name">{props.title}</div>
-                </div>
-                <nav className="nav">
-                    <ul className="navigation">
-                        <li className="inactive"><Link to="/">Home</Link></li>
-                        <li className="inactive"><Link to="/about">About Us</Link></li>
-                        <li className="inactive"><Link to="/products">Products</Link></li>
-                        <li className="inactive"><Link to="/services">Services</Link></li>
-                        <li className="inactive"><Link to="/our-work">Our Work</Link></li>
-                        <li className="inactive"><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </nav>
-            </header>
+  const [classN, setClassN] = useState("Home");
+  return (
+    <div>
+      <header className="header bb-8-green">
+        <div className="webName">
+          {props.logo ? (
+            <div className="logo">
+              <img src={logo} alt="Website Logo" />
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="web-name">{props.title}</div>
         </div>
-    )
-}
+        <nav className="nav">
+          <ul className="navigation">
+            <li
+              className={classN === "Home" ? "active" : "inactive"}
+              onClick={() => setClassN("Home")}
+            >
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              className={classN === "About Us" ? "active" : "inactive"}
+              onClick={() => setClassN("About Us")}
+            >
+              <Link to="/about">About Us</Link>
+            </li>
+            <li
+              className={classN === "Products" ? "active" : "inactive"}
+              onClick={() => setClassN("Products")}
+            >
+              <Link to="/products">Products</Link>
+            </li>
+            <li
+              className={classN === "Services" ? "active" : "inactive"}
+              onClick={() => setClassN("Services")}
+            >
+              <Link to="/services">Services</Link>
+            </li>
+            <li
+              className={classN === "Our Work" ? "active" : "inactive"}
+              onClick={() => setClassN("Our Work")}
+            >
+              <Link to="/our-work">Our Work</Link>
+            </li>
+            <li
+              className={classN === "Contact" ? "active" : "inactive"}
+              onClick={() => setClassN("Contact")}
+            >
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
